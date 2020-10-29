6d027916da1d6bcec0e797bdd46a4e283328af95
elizavetabelaya <elizavetabelaya@elizavetas-mbp>
2020-10-28 23:30:22 -0400

    Final Commit

--- Chess27/src/chess/Chess.java
+++ Chess27/src/chess/Chess.java
@@ -2,6 +2,7 @@ package chess;
 
 import figures.*;
 import java.util.Scanner;
+import java.util.*;
 /* 
  * @author Travis Harrell (tsh61)
  * @author Elizaveta Belaya (edb81)
@@ -19,17 +20,30 @@ import java.util.Scanner;
  //hello there
 public class Chess {
 	
+	/*
+	 * Handles the entire chess game. Contains main method which prints the board, records moves, displays a victory, etc.
+	 */
+	
 	//public static HashMap<String, String> board = new HashMap<String, String>(64); //81?
 	
 	public static Piece[][] board = new Piece[8][8];
 	
 	public static Piece[][] getBoard(){
+		
+		/*
+		 * Main method. Executes chess game.
+		 * 
+		 * @param  args  normal main method arguments
+		 */
+		
 		return Chess.board;
 	}
 	
 	public static int turnCount = 0;
 	
-	//test
+	public static String wKingPos;
+	public static String bKingPos;
+	
 	public static void main(String[] args) {
 		
 		//Piece p = new Pawn(false);
@@ -37,90 +51,147 @@ public class Chess {
 		initializeBoard();
 		
 		printBoard();
-		
+				
 		start();
+		
+		
 		
 	}
 	
 	public static void start() {
 		
+		/*
+		 * Plays the chess game, essentially. Frequently called within itself to start a new turn.
+		 */
+		
 		boolean end = false;
 		boolean turnColor = false;
 		
+		if(turnCount == 0) {
+			wKingPos = "e1";
+			bKingPos = "e8";
+		}
+		
+		
 		while(!end) {
-		
-		//System.out.println(turnCount);
-		
-		Scanner sc = new Scanner(System.in);
-		
-		
-		switch(turnCount%2) {
-			case(0):
-				System.out.println("White's move:");
-				turnColor = false;
-				break;
-			case(1):
-				System.out.println("Black's move:");
-				turnColor = true;
-				break;
-		}
 
-		String[] splitted = sc.nextLine().split("\\s+");
-		
-		if(splitted.length == 0) {
+			//System.out.println(turnCount);
+
+			Scanner sc = new Scanner(System.in);
+
+
+			switch(turnCount%2) {
+			case(0):
+			System.out.print("White's move: ");
+			turnColor = false;
+			break;
+			case(1):
+			System.out.print("Black's move: ");
+			turnColor = true;
+			break;
+			}
+
+			String[] splitted = sc.nextLine().split("\\s+");
+
+			System.out.println();
+
+			/*if(splitted.length == 0) {
 			System.out.println("Illegal move, try again");
+			System.out.println();
 			start();
-		}
-		
-		else if(splitted.length == 0 || splitted[0].length() < 2 || splitted.length > 4) {
+			}*/
+
+			if(splitted.length == 0 || splitted.length > 4) {
 			System.out.println("Illegal move, try again");
-			start();	
-		}
-		
-		else if(splitted[0].equals("resign")) {
-			
+			System.out.println();
+			start();
+			}
+
+			else if (splitted.length == 1) {
+			if(splitted[0].equals("resign")) {
 			if(turnCount%2 == 0) {
-				System.out.println("Black wins");
-				end = true;
+			System.out.println("Black wins");
+			end = true;
+			break;
+			}
+			else if(turnCount%2 == 1){
+			System.out.println("White wins");
+			end = true;
+			break;
+			}
 			}
 			else {
-				System.out.println("White wins");
-				end = true;
+			System.out.println("Illegal move, try again");
+			System.out.println();
+			start();
 			}
-		}
-		
-		else {
+			}
+
+			else if(splitted.length == 4) {
+			if(splitted[3].equals("draw?")) {
+			String d = sc.nextLine();
+			if(d == "draw") {
+			end = true;
+			}
+			break;
+			}
+			else {
+			System.out.println("Illegal move, try again");
+			System.out.println();
+			start();
+			}
+			}
+
+			else if(splitted.length >= 2) {
+			
 			int x1 = Character.getNumericValue(splitted[0].charAt(0)) - 10;
 			int y1 = 7 - Character.getNumericValue(splitted[0].charAt(1) - 1);
 			int x2 = Character.getNumericValue(splitted[1].charAt(0)) - 10;
 			int y2 = 7 - Character.getNumericValue(splitted[1].charAt(1) - 1);
 		
-			Piece initial = board[y1][x1];
-			//Piece destination = board[y2][x2];
-			
-			
-			
+			Piece initial = board[y1][x1];			
+						
 			if(initial == null || initial.getColor() != turnColor) {
 				System.out.println("Illegal move, try again");
 				start();
 			}
 		
 			boolean initialColor = initial.getColor();
-		
-			//Pawn test = new Pawn(true);
-		
-			//boolean output = test.getColor();
-		
-			//System.out.println(output);
-		
-		
+					
+
 			if(initial instanceof Pawn) {
 				initial = new Pawn(initialColor);
 				if(initial.validMove(splitted[0], splitted[1]) == true){
 					//System.out.println("true");
+					
 					board[y2][x2] = initial;
 					board[y1][x1] = null;
+					
 					printBoard();
+					
+					if(turnCount%2==0){//white   
+						
+						if(check(bKingPos)==true) {//do not need dest of pawn
+							System.out.println("Check");
+						}
+					}else if(turnCount%2!=0) {//black   
+
+						if(check(wKingPos)==true) {//do not need dest of pawn //e6 a3
+							System.out.println("Check");
+						}
+					}
+					
+			/*		if(turnCount%2==0){//white   
+						
+						if(checkMate(splitted[1],bKingPos)==true) {//do not need dest of pawn
+							System.out.println("Checkmate" + "\n" + "White wins");
+						}
+					}else if(turnCount%2!=0) {//black   
+
+						if(checkMate(splitted[1],wKingPos)==true) {//do not need dest of pawn //e6 a3
+							System.out.println("Checkmate" + "\n" + "Black wins");
+						}
+					}*/
 					turnCount++;
 					start();
 				}
@@ -135,9 +206,35 @@ public class Chess {
 				initial = new Knight(initialColor);
 				if(initial.validMove(splitted[0], splitted[1]) == true){
 					//System.out.println("true");
+					
+					
 					board[y2][x2] = initial;
 					board[y1][x1] = null;
+					
 					printBoard();
+					
+					if(turnCount%2==0){//white   
+						
+						if(check(bKingPos)==true) {//do not need dest of pawn
+							System.out.println("Check");
+						}
+					}else if(turnCount%2!=0) {//black   
+
+						if(check(wKingPos)==true) {//do not need dest of pawn //e6 a3
+							System.out.println("Check");
+						}
+					}
+					/*if(turnCount%2==0){//white   
+						
+						if(checkMate(splitted[1],bKingPos)==true) {//do not need dest of pawn
+							System.out.println("Checkmate" + "\n" + "White wins");
+						}
+					}else if(turnCount%2!=0) {//black   
+
+						if(checkMate(splitted[1],wKingPos)==true) {//do not need dest of pawn //e6 a3
+							System.out.println("Checkmate" + "\n" + "Black wins");
+						}
+					}*/
 					turnCount++;
 					start();
 				}
@@ -151,9 +248,33 @@ public class Chess {
 				initial = new Rook(initialColor);
 				if(initial.validMove(splitted[0], splitted[1]) == true){
 					//System.out.println("true");
+										
 					board[y2][x2] = initial;
 					board[y1][x1] = null;
+					
 					printBoard();
+					if(turnCount%2==0){//white   
+						
+						if(check(bKingPos)==true) {//do not need dest of pawn
+							System.out.println("Check");
+						}
+					}else if(turnCount%2!=0) {//black   
+
+						if(check(wKingPos)==true) {//do not need dest of pawn //e6 a3
+							System.out.println("Check");
+						}
+					}	
+				/*	if(turnCount%2==0){//white   
+						
+						if(checkMate(splitted[1],bKingPos)==true) {//do not need dest of pawn
+							System.out.println("Checkmate" + "\n" + "White wins");
+						}
+					}else if(turnCount%2!=0) {//black   
+
+						if(checkMate(splitted[1],wKingPos)==true) {//do not need dest of pawn //e6 a3
+							System.out.println("Checkmate" + "\n" + "Black wins");
+						}
+					}*/
 					turnCount++;
 					start();
 				}
@@ -166,10 +287,38 @@ public class Chess {
 			else if(initial instanceof Bishop) {
 				initial = new Bishop(initialColor);
 				if(initial.validMove(splitted[0], splitted[1]) == true){
-					//System.out.println("true");
+					//System.out.println("true");				
+					
 					board[y2][x2] = initial;
 					board[y1][x1] = null;
+					
+
+					
 					printBoard();
+					
+					if(turnCount%2==0){//white   
+						
+						if(check(bKingPos)==true) {//do not need dest of pawn
+							System.out.println("Check");
+						}
+					}else if(turnCount%2!=0) {//black   
+
+						if(check(wKingPos)==true) {//do not need dest of pawn //e6 a3
+							System.out.println("Check");
+						}
+					}	
+					
+				/*	if(turnCount%2==0){//white   
+						
+						if(checkMate(splitted[1],bKingPos)==true) {//do not need dest of pawn
+							System.out.println("Checkmate" + "\n" + "White wins");
+						}
+					}else if(turnCount%2!=0) {//black   
+
+						if(checkMate(splitted[1],wKingPos)==true) {//do not need dest of pawn //e6 a3
+							System.out.println("Checkmate" + "\n" + "Black wins");
+						}
+					}*/
 					turnCount++;
 					start();
 				}
@@ -183,9 +332,37 @@ public class Chess {
 				initial = new Queen(initialColor);
 				if(initial.validMove(splitted[0], splitted[1]) == true){
 					//System.out.println("true");
+					
+										
 					board[y2][x2] = initial;
 					board[y1][x1] = null;
+	
 					printBoard();
+					
+					if(turnCount%2==0){//white   
+						
+						if(check(bKingPos)==true) {//do not need dest of pawn
+							System.out.println("Check");
+						}
+					}else if(turnCount%2!=0) {//black   
+
+						if(check( wKingPos)==true) {//do not need dest of pawn //e6 a3
+							System.out.println("Check");
+						}
+					}
+					
+				/*	if(turnCount%2==0){//white   
+						
+						if(checkMate(splitted[1],bKingPos)==true) {//do not need dest of pawn
+							System.out.println("Checkmate" + "\n" + "White wins");
+						}
+					}else if(turnCount%2!=0) {//black   
+
+						if(checkMate(splitted[1],wKingPos)==true) {//do not need dest of pawn //e6 a3
+							System.out.println("Checkmate" + "\n" + "Black wins");
+						}
+					}*/
+					
 					turnCount++;
 					start();
 				}
@@ -197,13 +374,40 @@ public class Chess {
 			
 			else if(initial instanceof King) {
 				initial = new King(initialColor);
-				if(initial.validMove(splitted[0], splitted[1]) == true){
+				if(initial.validMove(splitted[0], splitted[1]) == true && underAttack(splitted[0], splitted[1])==false ){
 					//System.out.println("true");
-					board[y2][x2] = initial;
-					board[y1][x1] = null;
-					printBoard();
-					turnCount++;
-					start();
+				
+					/*if(turnCount%2==0){//white king position changes
+						wKingPos = splitted[1];
+						if(check( bKingPos)) {
+							System.out.println(splitted[0] + bKingPos + "CHECKKKKKKKK");
+						}
+					}else if(turnCount%2!=0) {//black king position changes
+						bKingPos = splitted[1];
+						if(check( wKingPos)) {
+							System.out.println(splitted[0] + bKingPos + "CHECKKKKKKKK");
+						}
+					}	*/
+						board[y2][x2] = initial;
+						board[y1][x1] = null;
+												
+						printBoard();
+						//System.out.println("White King Position: "+ wKingPos);
+						//System.out.println("Black King Position: "+ bKingPos);
+						if(turnCount%2==0){//white king position changes
+							wKingPos = splitted[1];
+							if(check(bKingPos)==true){
+								System.out.println("Check");
+							}
+						}else if(turnCount%2!=0) {//black king position changes
+							bKingPos = splitted[1];
+							if(check(wKingPos)==true){
+								System.out.println("Check");
+							}
+						}	
+						turnCount++;
+						start();
+						
 				}
 				else {
 					System.out.println("Illegal move, try again");
@@ -211,13 +415,276 @@ public class Chess {
 				}
 			}
 			
+			
+			
 			sc.close();
 			}
 		sc.close();
 		}
 	}
 	
-	public static void initializeBoard() {
+/*	public static boolean checkMate(String hz, String orig) {
+		int x1 = Character.getNumericValue(orig.charAt(0)) - 10;
+		int y1 = 7 - Character.getNumericValue(orig.charAt(1) - 1);
+		
+		int x2 = Character.getNumericValue(hz.charAt(0)) - 10;
+		int y2 = 7 - Character.getNumericValue(hz.charAt(1) - 1);
+		char a;
+		char b;
+		int count = 0;
+		int q = 0;
+		List <String> kingDest = new ArrayList <String> ();
+		
+		
+		//board[y1-1][x1]		
+		 a = convert(x1, 'j');
+		 b = convert(y1-1, 'i');	 
+		 if((a=='a'||a=='b'||a=='c'||a=='d'||a=='e'||a=='f'||a=='g'||a=='h')  &&  (b=='1'||b=='2'||b=='3'||b=='4'||b=='5'||b=='6'||b=='7'||b=='8')) {
+		 String dest1 = "" + a + b;	 
+		 kingDest.add(dest1);
+		 }	else {
+			 q++;
+		 }
+		 
+		//board[y1+1][x1]
+		 b = convert(y1+1, 'i');
+		 
+		 if((a=='a'||a=='b'||a=='c'||a=='d'||a=='e'||a=='f'||a=='g'||a=='h')  &&  (b=='1'||b=='2'||b=='3'||b=='4'||b=='5'||b=='6'||b=='7'||b=='8')) {
+		 String dest2 = "" + a + b;		
+		 kingDest.add(dest2);
+		 }else {
+			 q++;
+		 }
+		 
+		//board[y1][x1+1]
+		 a = convert(x1+1, 'j');
+		 b = convert(y1, 'i');
+		 
+		 if((a=='a'||a=='b'||a=='c'||a=='d'||a=='e'||a=='f'||a=='g'||a=='h')  &&  (b=='1'||b=='2'||b=='3'||b=='4'||b=='5'||b=='6'||b=='7'||b=='8')) {
+		 String dest3 = "" + a + b;	
+		 kingDest.add(dest3);
+		 }else {
+			 q++;
+		 }
+		//board[y1][x1+1]
+		 a = convert(x1-1, 'j');
+		 
+		 if((a=='a'||a=='b'||a=='c'||a=='d'||a=='e'||a=='f'||a=='g'||a=='h')  &&  (b=='1'||b=='2'||b=='3'||b=='4'||b=='5'||b=='6'||b=='7'||b=='8')) {
+		 String dest4 = "" + a + b;	
+		 kingDest.add(dest4);
+		 }else {
+			 q++;
+		 }
+		 
+		 
+		//board[y1-1][x1-1]
+		 a = convert(x1-1, 'j');
+		 b = convert(y1-1, 'i');
+		 if((a=='a'||a=='b'||a=='c'||a=='d'||a=='e'||a=='f'||a=='g'||a=='h')  &&  (b=='1'||b=='2'||b=='3'||b=='4'||b=='5'||b=='6'||b=='7'||b=='8')) {
+		 String dest5 = "" + a + b;
+		 kingDest.add(dest5);
+		 }else {
+			 q++;
+		 }
+		//board[y1-1][x1+1]
+		 a = convert(x1+1, 'j');	
+		 if((a=='a'||a=='b'||a=='c'||a=='d'||a=='e'||a=='f'||a=='g'||a=='h')  &&  (b=='1'||b=='2'||b=='3'||b=='4'||b=='5'||b=='6'||b=='7'||b=='8')) {
+		 String dest6 = "" + a + b;
+		 kingDest.add(dest6);
+		 }else {
+			 q++;
+		 }
+		//board[y1+1][x1-1]
+		 a = convert(x1-1, 'j');
+		 b = convert(y1+1, 'i');
+		 if((a=='a'||a=='b'||a=='c'||a=='d'||a=='e'||a=='f'||a=='g'||a=='h')  &&  (b=='1'||b=='2'||b=='3'||b=='4'||b=='5'||b=='6'||b=='7'||b=='8')) {
+		 String dest7 = "" + a + b;
+		 kingDest.add(dest7);
+		 }else {
+			 q++;
+		 }
+		//board[y1+1][x1+1]
+		 a = convert(x1+1, 'j');	
+		 if((a=='a'||a=='b'||a=='c'||a=='d'||a=='e'||a=='f'||a=='g'||a=='h')  &&  (b=='1'||b=='2'||b=='3'||b=='4'||b=='5'||b=='6'||b=='7'||b=='8')) {
+		 String dest8 = "" + a + b;
+		 kingDest.add(dest8);
+		 }else {q++;}
+		
+		 
+		 
+		 for (int i = 0; i < kingDest.size(); i++) {
+			 System.out.println(kingDest.get(i));
+		 }	 
+			 
+		 for (int i = 0; i < kingDest.size(); i++) {
+	    
+			 if(check(hz,kingDest.get(i)) == true) {
+				 count ++;
+			 }else if(check(hz,kingDest.get(i)) == false) {
+				 	q++;			 
+			 }
+	       }
+		 
+		 
+		 if (count == kingDest.size()) {
+			 return true;
+		 }
+		 
+		 return false;		 
+	}*/
+	
+	public static boolean check(String dest) { //a3		
+		
+		/*
+		 * Sees if king is in jeopardy of being captured.
+		 * 
+		 * @param  dest  destination of king 
+		 * @return     <code>true</code> if king in check, <code>false</code> otherwise
+		 */
+		
+		int x2 = Character.getNumericValue(dest.charAt(0)) - 10;
+		int y2 = 7 - Character.getNumericValue(dest.charAt(1) - 1);
+		
+		for(int i = 0; i < 8; i++){
+			for(int j = 0; j < 8; j++){			
+				Piece currPiece = board[i][j]; 			 				
+				
+				if(board[i][j]!=null){
+				//	System.out.println("check1");
+				//System.out.println(dest);
+					if( (currPiece.black==true && board[y2][x2].black==false) || (currPiece.black==false && board[y2][x2].black==true) ){					
+					//	System.out.println("check2");
+						char a = convert(j, 'j');
+						char b = convert(i, 'i');
+						String currPos = "" + a + b;//f8
+						if(currPiece.validMove(currPos, dest) == true){
+						//	System.out.println("check3");
+							return true;
+						}
+					}
+
+					}
+								
+			}
+		}
+		
+			
+			
+			
+		return false;
+	}
+	
+	public static boolean underAttack(String orig, String dest) {
+		
+		/*
+		 * Checks if king can move to a spot without being under the range of attack of another piece.
+		 * 
+		 * @param  orig  origin of king piece
+		 * @param  dest  destination of king piece
+		 * 
+		 * @return       <code>true</code> if king would be under attack if moved to destination, <code>false</code> otherwise
+		 */
+		
+		int x1 = Character.getNumericValue(orig.charAt(0)) - 10;
+		int y1 = 7 - Character.getNumericValue(orig.charAt(1) - 1);
+		
+		for(int i = 0; i < 8; i++){
+			for(int j = 0; j < 8; j++){			
+				Piece currPiece = board[i][j]; 			 
+				if(board[i][j]!=null){
+					if( (currPiece.black==true && board[y1][x1].black==false) || (currPiece.black==false && board[y1][x1].black==true) ){
+						char a = convert(j, 'j');
+						char b = convert(i, 'i');
+						String currPos = "" + a + b;											
+						if(currPiece.validMove(currPos, dest) == true){
+							return true;
+						}											
+					} 				
+				}
+			}
+		}			
+		return false;
+	}
+		
+	public static char convert(int v, char c) {
+		
+		/*
+		 * Converts array coordinate to string coordinate.
+		 * 
+		 * @param  v  vertical coordinate
+		 * @param  c  horizontal coordinate
+		 * 
+		 * @return    char of input to construct string
+		 */
+		
+		char a= 'x';
+		
+		if(c == 'i') {
+				
+			if(v==0) {
+				a = '8';
+				
+			}else if(v==1){
+				a = '7';
+
+			}else if(v==2){
+				a = '6';
+				
+			}else if(v==3){
+				a = '5';	
+				
+			}else if(v==4){
+				a = '4';
+				
+			}else if(v==5){
+				a = '3';
+				
+			}else if(v==6){
+				a = '2';
+				
+			}else if(v==7){
+				a = '1';
+				
+			}
+			
+		}else if(c == 'j') {	
+			
+			if(v==0) {
+				a = 'a';
+				
+			}else if(v==1){
+				a = 'b';
+				
+			}else if(v==2){
+				a = 'c';
+				
+			}else if(v==3){
+				a = 'd';	
+			
+			}else if(v==4){
+				a = 'e';
+				
+			}else if(v==5){
+				a = 'f';
+				
+			}else if(v==6){
+				a = 'g';
+				
+			}else if(v==7){
+				a = 'h';
+				
+			}	
+			}			
+		
+		return a;
+	}
+	
+ 	public static void initializeBoard() {
+ 		
+ 		/*
+ 		 * Sets up untouched chess board for new game. Method called once in main method before game begins.
+ 		 */
+ 		
 		board[0][0] = new Rook(true);	board[7][0] = new Rook(false);
 		board[0][1] = new Knight(true); board[7][1] = new Knight(false);
 		board[0][2] = new Bishop(true); board[7][2] = new Bishop(false);
@@ -238,6 +705,10 @@ public class Chess {
 	}
 	
 	public static void printBoard() {
+		
+		/*
+		 * Prints most recent board, as it currently stands. Reflects moves made upon it.
+		 */
 		
 		int count = 8;
 		
--- Chess27/src/figures/King.java
+++ Chess27/src/figures/King.java
@@ -1,6 +1,7 @@
 package figures;
 
 import chess.*;
+//import java.util.*;
 
 /* 
  * @author Travis Harrell (tsh61)
@@ -9,11 +10,126 @@ import chess.*;
 
 public class King extends Piece{
 	
+	/*
+	 * Class for piece King. Extends superclass Piece.
+	 */
+	
 	public King(boolean b) {
 		super.black = b;
 	}
 	
 	public boolean validMove(String orig, String dest) {
+		
+		/*
+		 * Determines if the desired move conforms to chess rules. Also performs castling.
+		 * 
+		 * @param  orig  string indicating origin coordinate (ex: a1)
+		 * @param  dest  string indicating destination coordinate
+		 * @return     <code>true</code> if move is allowed, <code>false</code> otherwise.
+		 */
+		
+		Piece[][] board = Chess.getBoard();
+		int x1 = Character.getNumericValue(orig.charAt(0)) - 10;
+		int y1 = 7 - Character.getNumericValue(orig.charAt(1) - 1);
+		int x2 = Character.getNumericValue(dest.charAt(0)) - 10;
+		int y2 = 7 - Character.getNumericValue(dest.charAt(1) - 1);
+		int count = 0;
+		
+		if(board[y2][x2]!=null){
+			if((board[y2][x2].black == false && board[y1][x1].black == false)|| (board[y2][x2].black == true && board[y1][x1].black == true)) {
+				//System.out.println("same color");
+			//	System.out.println("YYYYYY");
+			//	System.out.println("XXXX");
+				return false;
+			}
+		}
+		//castling case
+		
+		
+		//check if moved 
+		
+		
+		if(x1 == x2) { //vertical 
+			if(y1-1 == y2) { //one cell up			
+				board[y1][x1].hasMoved = true;	
+				return true;
+			}else if(y1+1 == y2) {//one cell down				
+				board[y1][x1].hasMoved = true;							
+				return true;
+			}else{
+				return false; //tries to move up or down more than one cell
+			}		
+		}else if(y1 == y2) {// horizontal
+			if(x1+1 == x2) { //one cell right
+				board[y1][x1].hasMoved = true;	
+				return true;
+			}else if(x1-1 == x2) {//one cell left
+				board[y1][x1].hasMoved = true;	
+				return true;
+				
+			}else if(x1+2==x2) {//castling to the right  SHORT
+				System.out.println("X");
+					if(board[y1][x1].black == false) { //WHITE
+						System.out.println("XX");
+						if (board[y1][x1].hasMoved == false && board[7][7].hasMoved == false && Chess.underAttack("e1","f1")==false )  {
+							board[7][5]= board[7][7];
+							board[7][7] = null;							
+							return true;
+						}
+			
+					}else if(board[y1][x1].black == true) {	//BLACK
+						if (board[y1][x1].hasMoved == false && board[0][7].hasMoved == false && Chess.underAttack("e8","f8")==false) {
+							board[0][5]= board[0][7];
+							board[0][7] = null;	
+							return true;
+						}
+					}
+			}else if (x1-2==x2) {// to the left LONG
+				
+					if(board[y1][x1].black == false) {//WHITE
+						if (board[y1][x1].hasMoved == false && board[7][0].hasMoved == false && Chess.underAttack("e1","d1")==false )  {
+							board[7][3]= board[7][0];
+							board[7][0] = null;	
+							return true;							
+						}
+						
+					}else if(board[y1][x1].black == true) {	//BLACK
+						if (board[y1][x1].hasMoved == false && board[0][0].hasMoved == false && Chess.underAttack("e8","d8")==false) {
+							board[0][3]= board[0][0];
+							board[0][0] = null;	
+							return true;
+						}					
+					}				
+			}else {
+				return false;
+			}
+			
+		}else if(Math.abs(y2-y1)==Math.abs(x2-x1)) {// diagonal
+			if(x1>x2 && y1>y2){ //up and left
+				if(x1-1==x2 && y1-1==y2) { //one cell up and left
+					board[y1][x1].hasMoved = true;	
+					return true;
+				}
+			}else if(x1<x2 && y1>y2){ // up and right
+				if(x1+1==x2 && y1-1==y2){ //one cell up and left
+					board[y1][x1].hasMoved = true;	
+					return true;
+				}
+			}else if(x1>x2 && y1<y2){ // down and left
+				if(x1-1==x2 && y1+1==y2){ // one cell down and left
+					board[y1][x1].hasMoved = true;	
+					return true;
+				}
+			}else if(x1<x2 && y1<y2){ // down and right
+				if(x1+1==x2 && y1+1==y2){ // one cell down and right
+					board[y1][x1].hasMoved = true;	
+					return true;
+				}
+			}			
+		}else {
+			return false;
+		}
+				
 		return false;
 	}
 	
